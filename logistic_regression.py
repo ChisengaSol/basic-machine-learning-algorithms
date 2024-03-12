fc82e18f5815eeb6cd942a81a5d0bb53799f56ff
ChisengaSol <solmanzambia@gmail.com>
2024-03-12 20:30:49 +0000

    provided the right values for the get_momentum function in linear reg removed unnecessary codes in linear reg class formatted the code for all three files(main.py, logistic_regration.py,linear_regression.py)

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,51 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 np.random.seed(0)
 
+
 class LogisticRegression:
-  def __init__(self, lr, n_epochs):
-      self.lr = lr
-      self.n_epochs = n_epochs
-      self.train_losses = []
-      self.w = None
+    def __init__(self, lr, n_epochs):
+        self.lr = lr
+        self.n_epochs = n_epochs
+        self.train_losses = []
+        self.w = None
 
-  def add_ones(self, x):
-      return np.hstack((np.ones((x.shape[0], 1)), x))
+    def add_ones(self, x):
+        return np.hstack((np.ones((x.shape[0], 1)), x))
 
-  def sigmoid(self, x):
-      return 1 / (1 + np.exp(-x @ self.w))
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x @ self.w))
 
-  def cross_entropy(self, x, y_true):
-      y_pred = self.sigmoid(x)
-      #epsilon value to prevent division by zero
-      epsilon = 1e-15
-      loss = -np.mean(y_true * np.log(y_pred + epsilon) + (1 - y_true) * np.log(1 - y_pred + epsilon))
-      return loss
+    def cross_entropy(self, x, y_true):
+        y_pred = self.sigmoid(x)
+        # epsilon value to prevent division by zero
+        epsilon = 1e-15
+        loss = -np.mean(
+            y_true * np.log(y_pred + epsilon)
+            + (1 - y_true) * np.log(1 - y_pred + epsilon)
+        )
+        return loss
 
-  def predict_proba(self, x):
-      x = self.add_ones(x)
-      return self.sigmoid(x)
+    def predict_proba(self, x):
+        x = self.add_ones(x)
+        return self.sigmoid(x)
 
-  def predict(self, x):
-    return (self.predict_proba(x) >= 0.5).astype(int)
+    def predict(self, x):
+        return (self.predict_proba(x) >= 0.5).astype(int)
 
-  def fit(self, x, y):
-      # Add ones to x
-      x = self.add_ones(x)
+    def fit(self, x, y):
+        # Add ones to x
+        x = self.add_ones(x)
 
-      # Initialize w to zeros vector
-      self.w = np.zeros((x.shape[1], 1))
+        # Initialize w to zeros vector
+        self.w = np.zeros((x.shape[1], 1))
 
-      for epoch in range(self.n_epochs):
-          ypred = self.sigmoid(x)
-          grad = (-1 / x.shape[0]) * (x.T @ (y - ypred))
-          self.w = self.w - self.lr * grad
-          loss = self.cross_entropy(x, y)
-          self.train_losses.append(loss)
+        for epoch in range(self.n_epochs):
+            ypred = self.sigmoid(x)
+            grad = (-1 / x.shape[0]) * (x.T @ (y - ypred))
+            self.w = self.w - self.lr * grad
+            loss = self.cross_entropy(x, y)
+            self.train_losses.append(loss)
 
-          if epoch % 100 == 0:
-              print(f'loss for epoch {epoch}: {loss}')
+            if epoch % 100 == 0:
+                print(f"loss for epoch {epoch}: {loss}")
 
-  def accuracy(self, y_true, y_pred):
-      acc = np.mean(y_true == y_pred) * 100
-      return acc
+    def accuracy(self, y_true, y_pred):
+        acc = np.mean(y_true == y_pred) * 100
+        return acc
--- main.py
+++ main.py
@@ -1,69 +1,88 @@
 import linear_regression, logistic_regression
 import numpy as np
 import matplotlib.pyplot as plt
+
 np.random.seed(0)
 
+
 # generate data
-def generate_data(n= 1000):
-  np.random.seed(0)
-  x = np.linspace(-5.0, 5.0, n).reshape(-1,1)
-  y= (29 * x + 30 * np.random.rand(n,1)).squeeze()
-  x = np.hstack((np.ones_like(x), x))
-  return x,y
+def generate_data(n=1000):
+    np.random.seed(0)
+    x = np.linspace(-5.0, 5.0, n).reshape(-1, 1)
+    y = (29 * x + 30 * np.random.rand(n, 1)).squeeze()
+    x = np.hstack((np.ones_like(x), x))
+    return x, y
 
-x,y= generate_data()
+
+x, y = generate_data()
 # check the shape
-print ((x.shape,y.shape))
+print((x.shape, y.shape))
 
-def split_data(x,y,train_perc=0.8):
-  N=x.shape[0]
-  train_size=round(train_perc*N)
-  x_train,y_train=x[:train_size,:],y[:train_size]
-  x_test,y_test=x[train_size:,:],y[train_size:]
-  return x_train,y_train,x_test,y_test
 
-x_train,y_train,x_test,y_test=split_data(x,y)
-print(f"x_train:{x_train.shape}, y_train: {y_train.shape}, x_test: {x_test.shape}, y_test: {y_test.shape}")
+def split_data(x, y, train_perc=0.8):
+    N = x.shape[0]
+    train_size = round(train_perc * N)
+    x_train, y_train = x[:train_size, :], y[:train_size]
+    x_test, y_test = x[train_size:, :], y[train_size:]
+    return x_train, y_train, x_test, y_test
 
-plt.plot(x_train[:,1],y_train)
+
+x_train, y_train, x_test, y_test = split_data(x, y)
+print(
+    f"x_train:{x_train.shape}, y_train: {y_train.shape}, x_test: {x_test.shape}, y_test: {y_test.shape}"
+)
+
+plt.plot(x_train[:, 1], y_train)
 plt.show()
 
-m = linear_regression.Linear_regression(0.01,10)
-batch_without_momentum =m.train_batch_gradient(x_train,y_train)
-m.plot_loss(batch_without_momentum,"Batch gradient without momentum")
+m = linear_regression.Linear_regression(0.01, 10)
+batch_without_momentum = m.train_batch_gradient(x_train, y_train)
+m.plot_loss(batch_without_momentum, "Batch gradient without momentum")
 
-batch_with_momentum =m.train_batch_gradient(x_train,y_train,with_momentum=True)
-m.plot_loss(batch_with_momentum,"Batch gradient with momentum")
+batch_with_momentum = m.train_batch_gradient(x_train, y_train, with_momentum=True)
+m.plot_loss(batch_with_momentum, "Batch gradient with momentum")
 
-sgd_without_momentum = m.train_sgd(x_train,y_train)
-m.plot_loss(sgd_without_momentum,"sdg without momentum")
+sgd_without_momentum = m.train_sgd(x_train, y_train)
+m.plot_loss(sgd_without_momentum, "sdg without momentum")
 
-sgd_with_momentum = m.train_sgd(x_train,y_train,with_momentum=True)
-m.plot_loss(sgd_with_momentum,"sdg with momentum")
+sgd_with_momentum = m.train_sgd(x_train, y_train, with_momentum=True)
+m.plot_loss(sgd_with_momentum, "sdg with momentum")
 
-mini_batch_without_momentum = m.minibatch_gradient_descent(x_train,y_train)
-m.plot_loss(mini_batch_without_momentum,"mini batch without momentum")
+mini_batch_without_momentum = m.minibatch_gradient_descent(x_train, y_train)
+m.plot_loss(mini_batch_without_momentum, "mini batch without momentum")
 
-mini_batch_with_momentum = m.minibatch_gradient_descent(x_train,y_train,with_momentum=True)
-m.plot_loss(mini_batch_with_momentum, "with momentum")
+mini_batch_with_momentum = m.minibatch_gradient_descent(
+    x_train, y_train, with_momentum=True
+)
+m.plot_loss(mini_batch_with_momentum, "mini batch with momentum")
 
 
 from sklearn.datasets import make_classification
-X_class, y_class = make_classification(n_samples= 100, n_features=2, n_redundant=0,
-                           n_informative=2, random_state=1,
-                           n_clusters_per_class=1)
 
-def split_data_1(x,y,train_perc=0.8):
-  N=x.shape[0]
-  idx=np.random.permutation(x.shape[0])
-  x,y=x[idx],y[idx]
-  train_size=round(train_perc*N)
-  x_train,y_train=x[:train_size,:],y[:train_size]
-  x_test,y_test=x[train_size:,:],y[train_size:]
-  return x_train,y_train,x_test,y_test
+X_class, y_class = make_classification(
+    n_samples=100,
+    n_features=2,
+    n_redundant=0,
+    n_informative=2,
+    random_state=1,
+    n_clusters_per_class=1,
+)
 
-x_train,y_train,x_test,y_test=split_data_1(X_class,y_class)
-print(f"x_train:{x_train.shape}, y_train: {y_train.shape}, x_test: {x_test.shape}, y_test: {y_test.shape}")
+
+def split_data_1(x, y, train_perc=0.8):
+    N = x.shape[0]
+    idx = np.random.permutation(x.shape[0])
+    x, y = x[idx], y[idx]
+    train_size = round(train_perc * N)
+    x_train, y_train = x[:train_size, :], y[:train_size]
+    x_test, y_test = x[train_size:, :], y[train_size:]
+    return x_train, y_train, x_test, y_test
+
+
+x_train, y_train, x_test, y_test = split_data_1(X_class, y_class)
+print(
+    f"x_train:{x_train.shape}, y_train: {y_train.shape}, x_test: {x_test.shape}, y_test: {y_test.shape}"
+)
 
 model = logistic_regression.LogisticRegression(lr=0.1, n_epochs=1000)
 model.fit(x_train, y_train)
