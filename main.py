095598c6207ec7632b8bb965eeb2bea3d2bb21eb
ChisengaSol <solmanzambia@gmail.com>
2024-03-15 22:17:05 +0000

    moved some functions from the main.py to utilities.py

--- main.py
+++ main.py
@@ -1,31 +1,13 @@
 import linear_regression, logistic_regression
 import numpy as np
 import matplotlib.pyplot as plt
+from utility import generate_data, split_data, split_data_1
 
 np.random.seed(0)
-
-
-# generate data
-def generate_data(n=1000):
-    np.random.seed(0)
-    x = np.linspace(-5.0, 5.0, n).reshape(-1, 1)
-    y = (29 * x + 30 * np.random.rand(n, 1)).squeeze()
-    x = np.hstack((np.ones_like(x), x))
-    return x, y
-
 
 x, y = generate_data()
 # check the shape
 print((x.shape, y.shape))
-
-
-def split_data(x, y, train_perc=0.8):
-    N = x.shape[0]
-    train_size = round(train_perc * N)
-    x_train, y_train = x[:train_size, :], y[:train_size]
-    x_test, y_test = x[train_size:, :], y[train_size:]
-    return x_train, y_train, x_test, y_test
-
 
 x_train, y_train, x_test, y_test = split_data(x, y)
 print(
@@ -67,17 +49,6 @@ X_class, y_class = make_classification(
     random_state=1,
     n_clusters_per_class=1,
 )
-
-
-def split_data_1(x, y, train_perc=0.8):
-    N = x.shape[0]
-    idx = np.random.permutation(x.shape[0])
-    x, y = x[idx], y[idx]
-    train_size = round(train_perc * N)
-    x_train, y_train = x[:train_size, :], y[:train_size]
-    x_test, y_test = x[train_size:, :], y[train_size:]
-    return x_train, y_train, x_test, y_test
-
 
 x_train, y_train, x_test, y_test = split_data_1(X_class, y_class)
 print(
---
+++ utility.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def generate_data(n=1000):
+    np.random.seed(0)
+    x = np.linspace(-5.0, 5.0, n).reshape(-1, 1)
+    y = (29 * x + 30 * np.random.rand(n, 1)).squeeze()
+    x = np.hstack((np.ones_like(x), x))
+    return x, y
+
+def split_data(x, y, train_perc=0.8):
+    N = x.shape[0]
+    train_size = round(train_perc * N)
+    x_train, y_train = x[:train_size, :], y[:train_size]
+    x_test, y_test = x[train_size:, :], y[train_size:]
+    return x_train, y_train, x_test, y_test
+
+def split_data_1(x, y, train_perc=0.8):
+    N = x.shape[0]
+    idx = np.random.permutation(x.shape[0])
+    x, y = x[idx], y[idx]
+    train_size = round(train_perc * N)
+    x_train, y_train = x[:train_size, :], y[:train_size]
+    x_test, y_test = x[train_size:, :], y[train_size:]
+    return x_train, y_train, x_test, y_test
