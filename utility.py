083f5583332cc920ddae9bc32edbebd37efa04d0
ChisengaSol <solmanzambia@gmail.com>
2024-03-16 11:54:27 +0000

    created one figure to contain all subplots in utility.py

--- utility.py
+++ utility.py
@@ -1,6 +1,7 @@
 import numpy as np
 from logistic_regression import LogisticRegression 
 from linear_regression import LinearRegression
+import matplotlib.pyplot as plt
 
 def generate_data(n=1000):
     np.random.seed(0)
@@ -28,24 +29,45 @@ def split_data_1(x, y, train_perc=0.8):
 def run_linear_regression(x_train, y_train,leaning_rate,n_epochs):
     m = LinearRegression(lr = leaning_rate,n_epochs = n_epochs)
     batch_without_momentum = m.train_batch_gradient(x_train, y_train)
-    m.plot_loss(batch_without_momentum, "Batch gradient without momentum")
 
     batch_with_momentum = m.train_batch_gradient(x_train, y_train, with_momentum=True)
-    m.plot_loss(batch_with_momentum, "Batch gradient with momentum")
 
     sgd_without_momentum = m.train_sgd(x_train, y_train)
-    m.plot_loss(sgd_without_momentum, "sdg without momentum")
 
     sgd_with_momentum = m.train_sgd(x_train, y_train, with_momentum=True)
-    m.plot_loss(sgd_with_momentum, "sdg with momentum")
 
     mini_batch_without_momentum = m.minibatch_gradient_descent(x_train, y_train)
-    m.plot_loss(mini_batch_without_momentum, "mini batch without momentum")
 
     mini_batch_with_momentum = m.minibatch_gradient_descent(
         x_train, y_train, with_momentum=True
     )
-    m.plot_loss(mini_batch_with_momentum, "mini batch with momentum")
+
+    #gradient descents to be plotted on one figure
+    #key is subplot index, list cotains type of gradient and its title
+    losses_plots = {1:[batch_without_momentum,"Batch gradient without momentum"],
+                    2:[batch_with_momentum,"Batch gradient with momentum"],
+                    3:[sgd_without_momentum,"Sdg without momentum"],
+                    4:[sgd_with_momentum,"Sgd with momentum"],
+                    5:[mini_batch_without_momentum,"Mini batch without momentum"],
+                    6:[mini_batch_with_momentum,"mini batch with momentum"],
+                    }
+     # Adjust size of the matplotlib figure window
+    fig = plt.figure(figsize=(10, 10)) 
+
+    for plt_idx, value in losses_plots.items():
+        fig.add_subplot(3, 2, plt_idx)
+        plt.plot(value[0])
+        plt.xlabel("epoch")
+        plt.ylabel("loss")
+        plt.title(value[1])
+
+    
+    
+    plt.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9, wspace=0.4, hspace=0.5)
+    plt.suptitle("Linear regression losses using different types of gradient descent")
+    plt.show()
+
+         
 
 def run_logistic_regression(x_train, y_train,leaning_rate,n_epochs):
     model = LogisticRegression(lr=leaning_rate ,n_epochs=n_epochs)
